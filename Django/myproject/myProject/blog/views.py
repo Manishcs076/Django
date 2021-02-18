b4a440756bbfac85720f01a01301a3b80fc1cb56
Manishcs076 <manishcs036@gmail.com>
2021-02-19 01:33:12 +0530

    updating some values in views

--- Django/myproject/myProject/blog/views.py
+++ Django/myproject/myProject/blog/views.py
@@ -4,19 +4,19 @@ from django.shortcuts import render
 
 posts = [
     {
-        'Name': "Manish Kumar Panday",
-        'Address': "Siwan Bihar",
-        'Email': "manishcs036@gmail.com",
-        'Mobile_number': 7781823530,
-        'Birthday': "14/10/1997",
+        'author': "Manish Kumar Panday",
+        'date_posted': "12/12/2035",
+        'title': "Broken Angles",
+        'content': "Drama",
+        
     },
     {
     
-        'Name': "Ashutosh Mishra",
-        'Address': "Lar Road Deoria",
-        'Email': "1516510042@kit.ac.in",
-        'Mobile_number': 8381858530,
-        'Birthday': "17/02/1996",
+        'author': "Ashutosh Mishra",
+        'date_posted': "31/12/2030",
+        'title': "How to free fall?",
+        'content': "Adventure",
+        
     }
     ]
 def home(request):
