24bf14d7635e9bfa7d7cac00d8078351c8972b9b
Manishcs076 <manishcs036@gmail.com>
2021-02-20 00:22:14 +0530

    add message by importing message

--- Django/myproject/myProject/users/views.py
+++ Django/myproject/myProject/users/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm #importing userCreationForm
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
-    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm()
+        if form.is_valid:
+            username = form.cleaned_data.get('username')
+            messages.success(request,f'Account created for {username} !')
+            return redirect('blog-home')
+    else:
+        form = UserCreationForm()
     return render(request,'users/register.html',{'form':form})
+
+
+
