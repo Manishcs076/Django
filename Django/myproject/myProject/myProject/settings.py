2ab84784866e5694f2cab4f7e19e20d32bdebb5c
Manishcs076 <manishcs036@gmail.com>
2021-02-20 15:32:46 +0530

    nothing change

--- Django/myproject/myProject/myProject/settings.py
+++ Django/myproject/myProject/myProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xad3sdx=d86xah7ic0i20i@!0uc231oql%4574h-dny!ediv=)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ ]
 
 
 # Application definition
