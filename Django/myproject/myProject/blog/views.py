70112a1bc0996852a247b7ce842a3aa802c6e0d7
Manishcs076 <manishcs036@gmail.com>
2021-02-21 22:40:27 +0530

    loginrequiredmixin

--- Django/myproject/myProject/blog/views.py
+++ Django/myproject/myProject/blog/views.py
@@ -20,11 +20,11 @@ class PostListView(ListView):
     ordering = ['-date_post']
 
 
-class PostDetailView(LoginRequiredMixin,DetailView):
+class PostDetailView(DetailView):
     model = Post
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin,CreateView):
     model = Post
     fields = ['title', 'content']
 
